bf2d7f1f5488842d12b10d5b166e69e429ff3a66
Ben Decato <decato@usc.edu>
2009-07-07 01:26:50 +0000

    Test without comments for simple polynomial differentiation. I'll add comments later.
    git-svn-id: https://svn.code.sf.net/p/calchelper/code/trunk@49 e19b9a60-eca0-4465-af06-84cbc52874d9

---
+++ tests/calchelper/tree/DifferentiationTest.java
@@ -0,0 +1,47 @@
+package calchelper.tree;
+
+import static org.junit.Assert.assertEquals;
+import java.util.HashMap;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * JUnit-based test harness for Polynomial.
+ *
+ * Written by Patrick McArthur
+ * Edited by Ben Decato
+ */
+
+public class DifferentiationTest
+{
+   // Expected results for each test
+   HashMap<Double,Double> expected;
+
+   // This exercises the Polynomial class
+   public static Polynomial intTest( String infix ) throws ExpressionException
+   {
+      TreeFactory factory;
+      ExpressionTree tree;
+      AbstractNode treeRoot;
+      Polynomial poly;
+
+      factory = new TreeFactory( infix );
+      tree = factory.buildTree();
+      treeRoot = tree.getRoot();
+      poly = new Polynomial( treeRoot );
+      poly.derive();
+      return poly;
+   }
+
+   @Before public void setUp()
+   {
+      expected = new HashMap<Double,Double>();
+   }
+   
+   @Test public void test1() throws ExpressionException
+   {
+      Polynomial result = intTest( "2 * x ^ 1" );
+      expected.put( 0.0, 2.0 );
+      assertEquals( result.map, expected );
+   }
+}
